775b6f7ca189ef2ff0a24bc58eb343ca4ef74f8b
rurcmug <155242828+rurcmug@users.noreply.github.com>
2023-12-30 14:54:27 -0600

    Patch 1-1 (#2)
    * Create install-packages.sh
    
    * added markdown-mode reference and install
    
    * Update README.md
    
    * Update README.md
    
    * added lua-mode.el
    
    * added dash to install script due to depencency from plantuml-mode
    
    ---------
    
    Co-authored-by: tmrcmug <155239368+tmrcmug@users.noreply.github.com>

--- install-packages.sh
+++ install-packages.sh
@@ -2,5 +2,6 @@
 
 curl https://raw.githubusercontent.com/jrblevin/markdown-mode/v2.5/markdown-mode.el -o elisp/markdown-mode.el
 curl https://raw.githubusercontent.com/immerrr/lua-mode/master/lua-mode.el -o elisp/lua-mode.el
+curl https://raw.githubusercontent.com/magnars/dash.el/master/dash.el -o elisp/dash.el
 curl https://raw.githubusercontent.com/skuro/plantuml-mode/develop/plantuml-mode.el -o elisp/plantuml-mode.el
 
